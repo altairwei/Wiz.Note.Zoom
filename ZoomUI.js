b6be2a57102e645d4179cb76269a841c98f13040
Altair Wei <altair_wei@outlook.com>
2019-05-19 10:36:22 +0800

    NEW: ported to WizNotePlus

--- ZoomUI.js
+++ ZoomUI.js
@@ -4,20 +4,46 @@ var pluginPath;
 var pluginData;
 var browserObject;
 var zoom = 100;
+let inited = false;
+
+// Initialize WizNote APIs
+new QWebChannel(qt.webChannelTransport, function (channel) {
+    var objectNames = ["WizExplorerApp", "JSPluginSpec", "JSPluginModuleSpec"];
+    for (var i = 0; i < objectNames.length; i++) {
+        var key = objectNames[i];
+        window[key] = channel.objects[key];
+    }
+    console.log("web channel opened");
+    //
+    if (typeof initForWebEngine !== 'undefined') {
+        try {
+            initForWebEngine();
+        } catch (err) {
+            console.error(err);
+        }
+    }
+});
 //
 async function initForWebEngine() {
     if (!window.WizExplorerApp) return;
+    if (inited) return;
     objApp = window.WizExplorerApp;
     objWindow = objApp.Window;
-    pluginData = window.WizPluginData;
+    pluginData = window.JSPluginSpec;
+    moduleData = window.JSPluginModuleSpec;
     pluginPath = pluginData.path;
-    browserObject = objWindow.CurrentDocumentBrowserObject;
+    browserObject = await objWindow.CurrentDocumentBrowserObject();
     await initZoom();
+    //
+    moduleData.willShow.connect(() => {
+        initZoom();
+    });
 }
-console.log("Note.Zoom inited");
+
 initForWebEngine();
 //
 async function initZoom() {
+    browserObject = await objWindow.CurrentDocumentBrowserObject();
     zoom = await browserObject.GetZoom();
     zoomValue.innerHTML = zoom + "%";
     //objApp.Window.UpdateToolButton("document", "ZoomButton", "/ButtonText=" + zoom + "%", "");
@@ -36,4 +62,8 @@ async function buttonZoomOut_onclick() {
         zoom = 50;
     await browserObject.SetZoom(zoom);
     initZoom();
+}
+
+async function resetBrowserObject() {
+    browserObject = objWindow.CurrentDocumentBrowserObject;
 }
